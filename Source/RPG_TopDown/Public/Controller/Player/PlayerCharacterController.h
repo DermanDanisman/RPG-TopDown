9300f0091da6cc1d11dc1322abb9cb3d9f4bae5f
Derman Can Danisman <dermancanwow@gmail.com>
2024-05-18 20:21:42 +0300

    Created Input and Controller Class for Player Character
    Input Action and Input Mapping Context
    Player Controller Class

---
+++ Source/RPG_TopDown/Public/Controller/Player/PlayerCharacterController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerCharacterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_TOPDOWN_API APlayerCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
