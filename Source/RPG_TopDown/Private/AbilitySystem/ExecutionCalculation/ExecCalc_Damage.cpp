9ae955c1030ad1ede11877c2624acc7086151c1d
Derman Can Danisman <dermancanwow@gmail.com>
2024-07-10 02:13:52 +0300

    Implemented Critical Hit
    Damage now changes with Critical Hit, Critical Damage and Critical Hit Resistance.

--- Source/RPG_TopDown/Private/AbilitySystem/ExecutionCalculation/ExecCalc_Damage.cpp
+++ Source/RPG_TopDown/Private/AbilitySystem/ExecutionCalculation/ExecCalc_Damage.cpp
@@ -14,6 +14,9 @@ struct TopDownDamageStatics
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(MagicResistance);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitChance);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitDamage);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitResistance);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Evasion);
 
 	TopDownDamageStatics()
@@ -21,6 +24,9 @@ struct TopDownDamageStatics
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, Armor, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, MagicResistance, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, ArmorPenetration, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, CriticalHitChance, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, CriticalHitDamage, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, CriticalHitResistance, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, Evasion, Target, false);
 	}
 };
@@ -36,6 +42,9 @@ UExecCalc_Damage::UExecCalc_Damage()
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 	RelevantAttributesToCapture.Add(DamageStatics().MagicResistanceDef);
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CriticalHitChanceDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CriticalHitDamageDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CriticalHitResistanceDef);
 	RelevantAttributesToCapture.Add(DamageStatics().EvasionDef);
 }
 
@@ -47,8 +56,11 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	AActor* SourceAvatarActor = SourceAbilitySystemComponent ? SourceAbilitySystemComponent->GetAvatarActor() : nullptr;
 	ICombatInterface* SourceCombatInterface = Cast<ICombatInterface>(SourceAvatarActor);
+	const UCharacterClassInfoDataAsset* SourceCharacterClassInfoDataAsset = UTopDownAbilitySystemLibrary::GetCharacterClassInfoDataAsset(SourceAvatarActor);
+	
 	AActor* TargetAvatarActor = TargetAbilitySystemComponent ? TargetAbilitySystemComponent->GetAvatarActor() : nullptr;
 	ICombatInterface* TargetCombatInterface = Cast<ICombatInterface>(TargetAvatarActor);
+	const UCharacterClassInfoDataAsset* TargetCharacterClassInfoDataAsset = UTopDownAbilitySystemLibrary::GetCharacterClassInfoDataAsset(TargetAvatarActor);
 	
 	const FGameplayEffectSpec GameplayEffectSpec = ExecutionParams.GetOwningSpec();
 
@@ -62,16 +74,6 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	// Get Damage Set by Caller Magnitude
 	float Damage = GameplayEffectSpec.GetSetByCallerMagnitude(FTopDownGameplayTags::Get().Damage);
 
-	// Capture Evade on Target, and determine if there was a successful Evasion.
-	float TargetEvasion = 0.f;
-	// We are getting the evasion value from the target
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().EvasionDef, EvaluationParameters, TargetEvasion);
-	TargetEvasion = FMath::Max<float>(TargetEvasion, 0.f);
-
-	const bool bEvaded = FMath::FRandRange(UE_SMALL_NUMBER, 100.f) <= TargetEvasion;
-	// if Target evades the attack, zero damage.
-	Damage = bEvaded ? Damage = 0.f : Damage;
-
 	// Armor penetration ignores a percentage of the target's armor
 	float TargetArmor = 0.f;
 	// We are getting the armor value from the target
@@ -83,22 +85,58 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	// We are getting the magic resistance value from the target
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().MagicResistanceDef, EvaluationParameters, TargetMagicResistance);
 	TargetMagicResistance = FMath::Max<float>(TargetMagicResistance, 0.f);
-	
+
 	float SourceArmorPenetration = 0.f;
 	// We are getting the armor penetration value from the source
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef, EvaluationParameters, SourceArmorPenetration);
 	SourceArmorPenetration = FMath::Max<float>(SourceArmorPenetration, 0.f);
 
-	const UCharacterClassInfoDataAsset* SourceCharacterClassInfoDataAsset = UTopDownAbilitySystemLibrary::GetCharacterClassInfoDataAsset(SourceAvatarActor);
+	float SourceCriticalHitChance = 0.f;
+	// We are getting the critical hit chance value from the source
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CriticalHitChanceDef, EvaluationParameters, SourceCriticalHitChance);
+	SourceCriticalHitChance = FMath::Max<float>(SourceCriticalHitChance, 0.f);
+
+	float SourceCriticalHitDamage = 0.f;
+	// We are getting the critical hit damage value from the source
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CriticalHitDamageDef, EvaluationParameters, SourceCriticalHitDamage);
+	SourceCriticalHitDamage = FMath::Max<float>(SourceCriticalHitDamage, 0.f);
+
+	float TargetCriticalHitResistance = 0.f;
+	// We are getting the critical hit damage value from the source
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CriticalHitResistanceDef, EvaluationParameters, TargetCriticalHitResistance);
+	TargetCriticalHitResistance = FMath::Max<float>(TargetCriticalHitResistance, 0.f);
+
+	// Capture Evade on Target, and determine if there was a successful Evasion.
+	float TargetEvasion = 0.f;
+	// We are getting the evasion value from the target
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().EvasionDef, EvaluationParameters, TargetEvasion);
+	TargetEvasion = FMath::Max<float>(TargetEvasion, 0.f);
+
+	const bool bEvaded = FMath::FRandRange(UE_SMALL_NUMBER, 100.f) <= TargetEvasion;
+	// if Target evades the attack, zero damage.
+	Damage = bEvaded ? Damage = 0.f : Damage;
+	
 	const FRealCurve* ArmorPenetrationCurve = SourceCharacterClassInfoDataAsset->DamageCalculationCoefficients->FindCurve(FName("ArmorPenetration"), FString());
 	const float ArmorPenetrationCoefficient = ArmorPenetrationCurve->Eval(SourceCombatInterface->GetCharacterLevel());
 
+	// Target Armor after Armor Penetration applied.
 	const float EffectiveArmor = TargetArmor * (100 - SourceArmorPenetration * ArmorPenetrationCoefficient) / 100.f;
-
-	const UCharacterClassInfoDataAsset* TargetCharacterClassInfoDataAsset = UTopDownAbilitySystemLibrary::GetCharacterClassInfoDataAsset(TargetAvatarActor);
+	
 	const FRealCurve* EffectiveArmorCurve = TargetCharacterClassInfoDataAsset->DamageCalculationCoefficients->FindCurve(FName("EffectiveArmor"), FString());
 	const float EffectiveArmorCoefficient = EffectiveArmorCurve->Eval(TargetCombatInterface->GetCharacterLevel());
+
+	// Armor ignores a percentage of incoming Damage
 	Damage *= (100 - EffectiveArmor * EffectiveArmorCoefficient) / 100.f;
+	
+	const FRealCurve* CriticalHitResistanceCurve = TargetCharacterClassInfoDataAsset->DamageCalculationCoefficients->FindCurve(FName("CriticalHitResistance"), FString());
+	const float CriticalHitResistanceCoefficient = CriticalHitResistanceCurve->Eval(TargetCombatInterface->GetCharacterLevel());
+
+	// Critical Hit Resistance reduces Critical Hit Chance by a certain percentage
+	const float EffectiveCriticalHitChance = SourceCriticalHitChance - TargetCriticalHitResistance * CriticalHitResistanceCoefficient;
+	const bool bCriticalHitChance = FMath::FRandRange(UE_SMALL_NUMBER, 100.f) <= EffectiveCriticalHitChance;
+
+	// Double damage plus a bonus if critical hit
+	Damage = bCriticalHitChance ? 2.f * Damage + SourceCriticalHitDamage : Damage;
 	
 	const FGameplayModifierEvaluatedData EvaluatedData(UBaseAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
